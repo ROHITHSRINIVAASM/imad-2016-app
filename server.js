5c413f74354558c1de8f4af9741ef96cac6555c1
ROHITHSRINIVAASM <mm16b008@smail.iitm.ac.in>
2016-09-28 11:50:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,23 +4,57 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone={
+ title:"Article one",
+ content:"this my first article",
+ date:"24 sep 16"
+};
+createtemplate=function(data)
+{var title=data.title;
+var content=data.content;
+var date=data.date;
+var htmltemplate=`<html>
+<head>
+    <title>
+    ${title}
+    </title>
+ 
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class=container>
+    <a href="/">Home</a>
+    <h1>
+        <p>
+            ${content}
+        </p>
+    </h1>
+    <h2>
+        <p>
+            ${date}
+        </p>
+    </h2>
+    </div>
+</body>
+</html>`;
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/articleone',function(req,res)
-{ res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+{ res.send(createtemplate(articleone));
     
 });
-app.get('/articletwo',function(req,res)
+/*app.get('/articletwo',function(req,res)
 {
      res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
 app.get('/articlethree',function(req,res)
 {
      res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
