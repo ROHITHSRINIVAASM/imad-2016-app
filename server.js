0fb1500bc2cce6f814bfb37379603cfe519129a7
ROHITHSRINIVAASM <mm16b008@smail.iitm.ac.in>
2016-10-25 21:17:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,12 +65,9 @@ app.get('/', function (req, res) {
 });
 var counter=0;
 app.get('/counter',function(req,res){
-   counter+=1;
+   counter=counter=1; 
    res.send(conter.toString());
 });
-
-
-
 app.get('/:articleName',function(req,res)
 
 {    var articleName=req.params.articleName;
