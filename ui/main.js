c6e913ccd4525c40946db9cbc4b1f86694c308dc
ROHITHSRINIVAASM <mm16b008@smail.iitm.ac.in>
2016-10-25 22:37:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,5 @@
-console.log('Loaded!');
+var counter=0;
+var button=document.getElementById('button');
+var span=document.getElementById('span');
+counter=counter+1;
+span.innerhtml=counter.toString();
