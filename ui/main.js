189b85bcc96968eb8fccf3ddf1666e10fa8e8dda
ROHITHSRINIVAASM <mm16b008@smail.iitm.ac.in>
2016-10-25 22:40:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,8 @@
 var counter=0;
 var button=document.getElementById('button');
+button.onclick=function(){
+
 var span=document.getElementById('span');
 counter=counter+1;
 span.innerhtml=counter.toString();
+};
