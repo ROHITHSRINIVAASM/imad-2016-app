62720c6ff9b806137892f12ff75596100a26255f
ROHITHSRINIVAASM <mm16b008@smail.iitm.ac.in>
2016-10-27 09:32:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,18 @@
 var counter=0;
 var button=document.getElementById('counter');
-button.onclick=function(){
+button.onclick=function()
+{
     
     var request=new XMLHttpRequest();
-     request.onreadystatechange=function(){
-         if(request.readystate===XMLHttpRequest.DONE)
+     request.onreadystatechange=function()
+     {
+         if(request.readyState===XMLHttpRequest.DONE)
          {if(request.status===200)
            {var counter=request.responseText;
-           var span=document.getElementById('count');
+            var span=document.getElementById('count');
             span.innerHTML=counter.toString();}
          }
      };
 requestopen('GET','http://rohithsrinivaasm.imad.hasura-app.io/counter',true);
-request.send(counter);
+request.send(null);
 };
