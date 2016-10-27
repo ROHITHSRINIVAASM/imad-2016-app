1884cec87185c52831d437d478480ad898c8cc24
ROHITHSRINIVAASM <mm16b008@smail.iitm.ac.in>
2016-10-27 11:14:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,27 @@ button.onclick=function()
 request.open('GET','http://rohithsrinivaasm.imad.hasura-app.io/counter',true);
 request.send(null);
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
+};
+
 var submit=document.getElementById('sumbit_btn');
 submit.onclick=function(){
-    var names=['kumaresh ramakrishnan','lokesh kumar','rishhanth maanav'];
+     var request=new XMLHttpRequest();
+     var nameInput=document.getElementById('name');
+      var name=nameInput.value;
+     request.onreadystatechange=function()
+     {
+         if(request.readyState===XMLHttpRequest.DONE)
+         {if(request.status===200)
+           { var names=request.responseText;
+            names=JSON.parse(names);
+               
+           }
+         }
+     };
+request.open('GET','http://rohithsrinivaasm.imad.hasura-app.io/sumbit?name='+name,true);
+request.send(null);
+    var names=[];
     var list='';
     for(var i=0;i<names.length;i++){
     list+='<li>' + names[i] + '</li>';
